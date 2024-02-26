5013b21a1eaab83137efb1c0184f41a3789271cf
Tomáš Zahradník <>
2024-02-26 18:44:20 +0100

    Forward, rekurze se 'snad spravne' funkcni konekci do predchozich neuronu

--- neat.py
+++ neat.py
@@ -14,6 +14,8 @@ class Node:
         self.hidden = hidden
         self.input = input
         self.bias = bias
+        self.hledaInput = False
+        self.ukoncenoHledani = 0
 
 
 class Manager:  # TODO druhy, mutace, crossover, celkove lepsi implementace, nez pokus o ni po precteni papiru bez mysleni o tom
@@ -71,40 +73,50 @@ class Sit:
             self.listNodu[i] = Node(hidden=False, input=i <= pocetInputu)
 
     def Forward(self, vstup):
-        nodes = []  # musi to byt list a ne mnozina, protoze mnozina je 'unordered' podle dokumentace, nevim zda me to ovlivnuje ale nechci debugovat duchy, tak radeji iteruji pres 1 if
-        nodes.append(self.listNodu[0])
         for node in range(1, self.pocetInputu + 1):
             self.listNodu[node].hodnota = vstup[node - 1]
-            nodes.append(self.listNodu[node])
-        self.SkutecnyForward(nodes, self.krokID)
+        for node in range(0, self.pocetInputu + 1):
+            self.SkutecnyForward(self.listNodu[node], self.krokID)
         output = []
         for i in range(self.pocetInputu + 1, self.pocetOutputu + self.pocetInputu + 1):
             output.append(self.listNodu[i].hodnota)
         self.krokID *= -1
         return output
 
-    def SkutecnyForward(self, nodes,
-                        krokID):  # je to vice Forward nez Forward, ale chci aby Forward se jmenoval Forward, takze toto bude SkutecnyForward, tentokrat dokonce i spravnejsi a predpovidatelnejsi nez kdy byl s rekurzi
-        while len(nodes) > 0:
-            nodesTemp = []
-            for node in nodes:
-                if node.krokID == krokID:
-                    continue
-                node.krokID = krokID
-                if not node.input:
-                    node.hodnota = 0
-                for Synapsa in node.SynapsyListInp:
-                    node.hodnota += Synapsa.hodnota
-                if node.hidden:
-                    node.hodnota = np.tanh(node.hodnota)
-                for Synapsa in node.SynapsyListOut:
-                    Synapsa.krokID = krokID
-                    if Synapsa.povolen:
-                        Synapsa.hodnota = Synapsa.vaha * node.hodnota
-                        nodesTemp.append(self.listNodu[Synapsa.output])
-                if node.bias:
-                    node.hodnota = 1.0
-            nodes = nodesTemp.copy()
+    def SkutecnyForward(self, node,
+                        krokID, otecHledaInput=False):  # je to vice Forward nez Forward, ale chci aby Forward se jmenoval Forward, takze toto bude SkutecnyForward
+        if node.krokID == krokID:
+            return
+        node.krokID = krokID
+        if not node.input and node.ukoncenoHledani == 0:
+            node.hodnota = 0
+        node.hledaInput = True
+        for i in range(node.ukoncenoHledani, len(node.SynapsyListInp)):
+            Synapsa = node.SynapsyListInp[i]
+            if Synapsa.krokID != krokID and Synapsa.povolen:
+                if self.listNodu[Synapsa.input].hledaInput:
+                    node.krokID *= -1
+                    node.ukoncenoHledani = i
+                    return 2
+                tempcislo = self.SkutecnyForward(self.listNodu[Synapsa.input], krokID, True)
+                if tempcislo == 2 and otecHledaInput:
+                    node.krokID *= -1
+                    node.ukoncenoHledani = i
+                    return 2
+            node.hodnota += Synapsa.hodnota
+        node.ukoncenoHledani = 0
+        node.hledaInput = False
+        if node.hidden:
+            node.hodnota = np.tanh(node.hodnota)
+        for Synapsa in node.SynapsyListOut:
+            Synapsa.krokID = krokID
+            if Synapsa.povolen:
+                Synapsa.hodnota = Synapsa.vaha * node.hodnota
+        for Synapsa in node.SynapsyListOut:  # pokud pujde rovnou do rekurze, tak nastane problem kdy node uz je oznacen jako hotovy, ale jeste nedal hodnotu synapse, kterou pouzivame
+            if Synapsa.povolen:
+                self.SkutecnyForward(self.listNodu[Synapsa.output], krokID)
+        if node.bias:
+            node.hodnota = 1.0
 
     def PridejSynapsu(self, Synapsa):
         if Synapsa.input == Synapsa.output:  # pokud by si treba bias neuron udelal primou konekci sam na sebe, tak by jeho output zacal nekontrolovatelne rust
